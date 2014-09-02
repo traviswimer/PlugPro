eedb4486d6276fe2769754da8c925117c5dbfde6
Travis Wimer <accounts@traviswimer.com>
2014-09-02 15:40:18 -0400

    Make extension runnable

---
+++ src/js/event_page.js
@@ -0,0 +1,31 @@
+chrome.runtime.onInstalled.addListener(function(install){
+	console.log(install);
+
+	// Detect as update by default
+	var injectFileName = "on_update.js";
+
+	// Check if it is actually the first install
+	if( install.reason === "install" ){
+		injectFileName = "on_install.js";
+	}
+
+	chrome.tabs.query(
+		{
+			"url": "*://plug.dj/*"
+		},
+		function(tabs){
+			for( var tabCount=0; tabCount<tabs.length; tabCount++){
+				chrome.tabs.executeScript(
+					tabs[tabCount].id,
+					{
+						"file": "scripts/" + injectFileName
+					},
+					function(injectback){
+						console.log("Injected");
+						console.log(injectback);
+					}
+				);
+			}
+		}
+	);
+});
