6d4e9d4f0070c175784a4c570d648ab02ff2b189
Travis Wimer <accounts@traviswimer.com>
2014-09-03 23:50:40 -0400

    Nothing particularly productive

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,16 @@
 
 var Room = require('./room');
 var room = new Room();
-room.on("load", function(){
 
+var Toggle = require('./settings/Toggle');
+var AutoWootToggle = require('./settings/AutoWootToggle');
+room.on("load", function(){
+	console.log("LOADED!");
+	var userId = API.getUser().id;
+	var toggler = new Toggle();
+	toggler.on();
+	new AutoWootToggle( toggler, userId );
+
+	setTimeout( toggler.on.bind(toggler), 15000);
 });
 room.init();
