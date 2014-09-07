fa4dcd390851468ee4e6a316125c55c06091d8a4
Travis Wimer <accounts@traviswimer.com>
2014-09-06 21:17:39 -0400

    Add menu tests

--- test/unit/ui/Menu_test.js
+++ test/unit/ui/Menu_test.js
@@ -5,16 +5,17 @@ var expect = chai.expect;
 
 var requireHelper = require('../../require_helper');
 var Menu = requireHelper('ui/Menu');
+var JST = requireHelper('ui/templates').JST;
 
 describe("Menu", function(){
 
 	var menu;
-	var JST = {};
-	JST['src/html_templates/menu.html'] = function(){return "";};
 
 	beforeEach(function(){
 		$('body').html("<div id='app-menu'><div class='list'></div></div><div id='plugpro-menu'></div>");
-		menu = new Menu( JST );
+		menu = new Menu( JST, {
+			"fakeToggleSetting": {}
+		});
 	});
 
 	it("should show and hide", function(){
@@ -26,6 +27,10 @@ describe("Menu", function(){
 		$('.list').css('left', '10px');
 		menu.toggle();
 		expect( $('#plugpro-menu').hasClass('expanded') ).to.be.false;
+	});
+
+	it("should add toggle settings", function(){
+		expect( $('#pro-toggle-settings').find('li').length ).to.equal(1);
 	});
 
 	describe("getTransitionEvent()", function(){
