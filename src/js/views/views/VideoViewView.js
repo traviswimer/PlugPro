685bcf94de73a6d723c9fc568e8967f40e69e47d
Travis Wimer <accounts@traviswimer.com>
2014-10-03 15:39:48 -0400

    Add video view fadeout when inactive

--- src/js/views/views/VideoViewView.js
+++ src/js/views/views/VideoViewView.js
@@ -13,7 +13,30 @@ var VideoViewView = {
 	* Renders the "video" view
 	*/
 	render: function(){
+
+		this.activeTimeout;
+		$('#playback-container').append("<div id='video-cover'></div>");
+
 		$('body').addClass('plugpro-video');
+
+		$('body, #playback-container').on( "mousemove", this.makeActive.bind(this) );
+
+		this.makeActive();
+	},
+
+	makeActive: function(){
+		console.log("active");
+		$('body').removeClass('inactive');
+		if( this.activeTimeout ){
+			clearTimeout( this.activeTimeout );
+		}
+
+		this.activeTimeout = setTimeout( this.makeInactive.bind(this), 2000 );
+	},
+
+	makeInactive: function(){
+		console.log("inactive");
+		$('body').addClass('inactive');
 	}
 
 };
