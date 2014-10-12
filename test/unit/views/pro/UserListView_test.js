d2c91524fa3d6d5113da170831ff367e0df6a2c2
Travis Wimer <accounts@traviswimer.com>
2014-10-12 14:49:57 -0400

    Add UserListView initialize test

---
+++ test/unit/views/pro/UserListView_test.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var chai = require('chai');
+var expect = chai.expect;
+var sinon = require("sinon");
+
+var rewire = require('../../../rewire_helper');
+var UserListView = rewire('views/pro/UserListView');
+
+window.plugPro = {};
+Backbone.$ = $;
+
+describe("UserListView", function(){
+
+	var userListView;
+	var fakeTemplate = "fake_template";
+
+	beforeEach(function(){
+		window.plugPro.JST['userlist.html'] = function(){
+			return fakeTemplate;
+		};
+		userListView = new UserListView();
+	});
+
+	describe("initialize()", function(){
+
+		it("should define html template", function(){
+			expect( userListView.historyHTML ).to.equal( fakeTemplate );
+		});
+
+	});
+
+});
