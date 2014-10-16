256b562dec2adc753c1b547d4eb80ca73aafead7
Travis Wimer <accounts@traviswimer.com>
2014-10-16 18:24:20 -0400

    Add jsDoc comments for PlayHistoryView

--- src/js/views/pro/PlayHistoryView.js
+++ src/js/views/pro/PlayHistoryView.js
@@ -4,18 +4,29 @@
  */
 var panes = require('./panes');
 
-var PlayHistoryView = Backbone.View.extend({
-
-	id: "plugpro-play-history",
-	className: "media-list history",
-
+var PlayHistoryView = Backbone.View.extend(
+/** @lends PlayHistoryView.prototype */
+{
+	/**
+	* Adds DOM elements and functionality for play history in the Pro-View
+	* @class PlayHistoryView
+	*
+	* @augments Backbone.View
+	* @constructs
+	*/
 	initialize: function(){
 		var JST = window.plugPro.JST;
 		this.historyHTML = JST['play_history.html'];
 
 		API.on( API.HISTORY_UPDATE, this.render.bind(this) );
 	},
+
+	id: "plugpro-play-history",
+	className: "media-list history",
 	
+	/**
+	* Render the PlayHistoryView
+	*/
 	render: function(){
 		this.$el.html("");
 
@@ -26,6 +37,9 @@ var PlayHistoryView = Backbone.View.extend({
 		}
 	},
 
+	/**
+	* Adds song to the DOM
+	*/
 	appendSong: function( songInfo ){
 
 		var historyDiv = this.historyHTML( songInfo );
@@ -33,10 +47,16 @@ var PlayHistoryView = Backbone.View.extend({
 		this.$el.append( historyDiv );
 	},
 
+	/**
+	* Destory the PlayHistoryView
+	*/
 	destroy: function(){
 		this.remove();
 	},
 
+	/**
+	* Move DOM elements to the correct positions
+	*/
 	reposition: function(){
 		var windowWidth = $(window).width();
 		var windowHeight = $(window).height();
